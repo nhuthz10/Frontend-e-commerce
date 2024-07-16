33783711eadee964b3f628edcacb426a4e5ddda2
nhuthz10 <nhuthz10@gmail.com>
2024-07-17 01:42:19 +0700

    fix tawk.to

--- src/layout/Footer/Footer.js
+++ src/layout/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Footer.scss";
+import Script from "next/script";
 
 function Footer() {
   return (
@@ -42,19 +43,10 @@ function Footer() {
           <p>Chính sách vận chuyển</p>
         </div>
       </div>
-      <script type="text/javascript">
-        {`
-          var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-          (function(){
-            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-            s1.async=true;
-            s1.src='https://embed.tawk.to/6696b87132dca6db2cb0d2a9/1i2uc0ut4';
-            s1.charset='UTF-8';
-            s1.setAttribute('crossorigin','*');
-            s0.parentNode.insertBefore(s1,s0);
-          })();
-    `}
-      </script>
+      <Script
+        strategy="lazyOnload"
+        src="https://embed.tawk.to/6696b87132dca6db2cb0d2a9/1i2uc0ut4"
+      />
     </>
   );
 }
