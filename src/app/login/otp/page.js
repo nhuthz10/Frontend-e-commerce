d254d326069f9c89d8fd9f0c62b2c0f97360cdf9
quynhlt2482 <thanhquynhdhsp@gmail.com>
2024-08-10 09:14:02 +0700

    ui otp

--- src/app/login/otp/page.js
+++ src/app/login/otp/page.js
@@ -1,8 +1,6 @@
 "use client"
 import React, { useState, useEffect } from 'react';
 import "./page.scss";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShield } from '@fortawesome/free-solid-svg-icons';
 import { toast } from "react-toastify";
 import { useRouter } from "next/navigation";
 import { useDispatch } from "react-redux";
@@ -10,9 +8,9 @@ import {
     logIn,
     updateFavourites,
     updateCartId,
-    logOut,
 } from "../../../redux-toolkit/userSlice";
 import { handleCreatCartService } from "../../../services/cartService";
+import Image from "next/image";
 const OtpSmsPage = () => {
     const [otp, setOtp] = useState(new Array(6).fill(""));
     const otpCode = localStorage.getItem('otpCode');
@@ -71,25 +69,33 @@ const OtpSmsPage = () => {
     };
 
     return (
-        <div className="otpInputContainer">
-            <FontAwesomeIcon className="otpInput-Icon" icon={faShield} size="10x" />
-            <h1 className="otpInput-Header">
-                VUI LÒNG NHẬP MÃ OTP 👋
-            </h1>
-            <div>
-                {otp.map((data, index) => (
-                    <input
-                        className="otpInput"
-                        type="text"
-                        name="otp"
-                        maxLength="1"
-                        key={index}
-                        value={data}
-                        onChange={e => handleChange(e.target, index)}
-                        onKeyDown={e => handleKeyDown(e, index)}
-                        onFocus={e => e.target.select()}
-                    />
-                ))}
+        <div className="otpInput-Body">
+            <div className="otpInputContainer">
+                <Image
+                    src="/images/shield-ok-icon.png"
+                    width={200}
+                    height={200}
+                    alt="img"
+                    className="otpInput-Icon"
+                />
+                <h1 className="otpInput-Header">
+                    VUI LÒNG NHẬP MÃ OTP 👋
+                </h1>
+                <div>
+                    {otp.map((data, index) => (
+                        <input
+                            className="otpInput"
+                            type="text"
+                            name="otp"
+                            maxLength="1"
+                            key={index}
+                            value={data}
+                            onChange={e => handleChange(e.target, index)}
+                            onKeyDown={e => handleKeyDown(e, index)}
+                            onFocus={e => e.target.select()}
+                        />
+                    ))}
+                </div>
             </div>
         </div>
     );
